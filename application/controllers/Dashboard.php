711a145872093dca1ae264001c28d15f7af30aa7
Surya Wijanarko <kokowijanarko0228@gmail.com>
2017-08-13 21:23:28 +0700

    new database

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -33,5 +33,86 @@ class Dashboard extends CI_Controller {
 	}
 	
 	
+	///hanya digunakan sekali untuk migrasi data
+	public function prod(){
+		$data = $this->dasboard_model->getProduct();
+		
+		foreach($data as $idx=>$val){
+			foreach($val as $key=>$value){
+				$params[$idx][$key] = $value;
+			}
+		}
+		// var_dump($params);
+		// die;
+		$this->db->trans_start();
+		$input = $this->db->insert_batch('produk', $params);
+		var_dump($input);
+		$this->db->trans_complete($input);
+		
+	}
+	
+	public function dp(){
+		$data = $this->dasboard_model->getPenjualanDetail();
+		
+		foreach($data as $idx=>$val){
+			foreach($val as $key=>$value){
+				$params_detpenj[$idx][$key] = $value;
+			}			
+			$id_penj[] = $val->id_penj		;	
+		}
+		
+		$id_penj = array_unique($id_penj);
+		
+		$id = '';
+		foreach($id_penj as $key=>$val){
+			$id .= $val.', ';
+		}
+		$id = rtrim($id, ', ');
+		// $id .= ')'; 
+		
+		// var_dump($id);die;
+		
+		$data_penj = $this->dasboard_model->getPenjualan($id);
+		
+
+		$asd = 11;
+		foreach($data_penj as $idx=>$val){
+			$params_penj[$idx]['kode_invoice'] = 'INV.'. $asd .'/'. date('d/M/Y', strtotime($val->tgl_trans));
+			foreach($val as $key=>$value){
+				$params_penj[$idx][$key] = $value;
+			}
+			$asd++;
+		}
+		//var_dump($params_penj, $params_detpenj);die;
+		
+		$this->db->trans_start();
+		$input = $this->db->insert_batch('penjualan', $params_penj);
+		if($input){
+			$input = $input && $this->db->insert_batch('detail_penjualan', $params_detpenj);
+		}
+		var_dump($input);
+		$this->db->trans_complete($input);
+		
+	}
+	
+	public function cust(){
+		$data = $this->dasboard_model->getCust();
+		
+		foreach($data as $idx=>$val){
+			foreach($val as $key=>$value){
+				$params[$idx][$key] = $value;
+			}
+		}
+		// var_dump($params);
+		// die;
+		
+		$this->db->trans_start();
+		$input = $this->db->insert_batch('customer', $params);
+		var_dump($input);
+		$this->db->trans_complete($input);
+		
+	}
+	
+	
 	
 }
--- application/models/Dasboard_model.php
+++ application/models/Dasboard_model.php
@@ -32,4 +32,80 @@ class Dasboard_model extends CI_Model
 		return $result;
 	}
 	
+	public function getProduct(){
+		$old = $this->load->database('old', true);	
+		$query = $old->query("
+			SELECT
+				b.`id` AS id_prod,
+				b.`productname` AS nama_prod,
+				IF(LOWER(d.`catname`) = 'gamis', 1, 2) AS jenis_prod,
+				b.`productsellprice` AS harga,
+				'50' AS stok
+			FROM prod_products b
+			LEFT JOIN prod_ref_category c ON c.`id` = b.`catid`
+			LEFT JOIN prod_ref_category d ON d.`id` = c.`parentcat`
+			WHERE
+			1=1
+			AND c.`parentcat` IN(10, 520, 564)
+		");
+		$result = $query->result();
+		return $result;
+	}
+	
+	public function getPenjualanDetail(){
+		$old = $this->load->database('old', true);	
+		$query = $old->query('
+			SELECT
+				a.`productid` AS id_prod,
+				a.`orderid` AS id_penj,
+				a.`ordersum` AS jml_jual,
+				(a.`ordersum` * b.`productsellprice`) AS total
+			FROM ord_products a
+			LEFT JOIN prod_products b ON b.`id` = a.`productid`
+			LEFT JOIN prod_ref_category c ON c.`id` = b.`catid`
+			LEFT JOIN prod_ref_category d ON d.`id` = c.`parentcat`
+			WHERE
+			1=1
+			AND c.`parentcat` IN(10, 520, 564)
+		');
+		$result = $query->result();
+		return $result;
+	}
+	
+	public function getPenjualan($id){
+		$old = $this->load->database('old', true);	
+		$query = $old->query("
+			SELECT
+				a.`id` AS id_penj,
+				'1' AS id_user,
+				a.`customerid` AS id_cust,
+				a.`orderdate` AS tgl_trans,
+				a.`ordershippingcost` AS biaya_kirim,
+				a.`ordertotaldiscpercent` AS diskon,
+				(SELECT SUM(ordertotal) FROM ord_products WHERE orderid = a.`id`) AS ttl_byr
+			FROM ord_orders a
+			WHERE a.`id` IN($id);
+		");
+		
+		$result = $query->result();
+		return $result;
+	}
+	
+	public function getCust(){
+		$old = $this->load->database('old', true);	
+		$query = $old->query("
+			SELECT
+				a.`id` AS id_cust,
+				a.`customername` AS nama_cust,
+				a.`customeraddress` AS alamat_cust,
+				a.`customerphone1` AS nohp_cust
+			FROM core_customers a
+		");
+		
+		$result = $query->result();
+		return $result;
+	}
+	
+	
+	
 }
